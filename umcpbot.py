42693f6bf2c9b1aa711970cfc817a48f9b9c23bb
vibhusomeshwar <redwallguy@gmail.com>
2018-07-21 18:06:55 -0400

    Removed db use for testing

--- umcpbot.py
+++ umcpbot.py
@@ -23,54 +23,6 @@ def remind(aid, message):
     requests.post(os.environ.get("WEBHOOK_URL"),headers={'Content-Type': 'application/json'},
                       data={'content': message})
 
-
-
-class Games:
-    def __init__(self):
-        cur.execute("SELECT * FROM games;")
-        glist = cur.fetchall()
-        self.games = {}
-        for g in glist:
-            self.games[g[0]] = []
-        for key in self.games:
-            cur.execute("SELECT (alias) FROM aliases WHERE game = %s", (key,))
-            alilist = cur.fetchall()
-            for ali in alilist:
-                self.games[key].append(ali[0])
-
-    def get_games(self):
-        return self.games
-
-    def add_game(self, game):
-        try:
-            cur.execute("INSERT INTO (games) VALUES (%s)", (game,))
-            self.games[game] = []
-        except:
-            return "Game already exists."
-
-    def add_alias(self, alias, game):
-        try:
-            cur.execute("INSERT INTO aliases (alias, game) VALUES (%s,%s)", (alias, game))
-            self.games[game].append(alias)
-        except:
-            return "Oops! Something went wrong."
-
-    def remove_alias(self, alias, game):
-        try:
-            cur.execute("DELETE FROM aliases WHERE alias = %s", (alias,))
-            self.games[game].remove(alias)
-        except:
-            return
-
-    def remove_game(self, game):
-        try:
-            cur.execute("DELETE FROM games WHERE game = %s", (game,))
-            self.games.pop(game, None)
-        except:
-            return
-
-gameObj = Games()
-
 async def no_reminder(ctx):
     numrem = r.lrem("reminderlist", ctx.author.id)
     if numrem != 0:
@@ -89,72 +41,6 @@ async def on_member_join(member):
     msg += " using the !addgame command. Once you do so, you can view and interact with that game's"
     msg += " voice and text channels, and " + readmechan.mention + " will disappear."
     await member.send(msg)
-
-@bot.command()
-async def addgame(ctx, *games):
-    """
-    Adds game(s) to your roles.
-
-    Usage: !addgame game1 game2 ...
-    Maximum 10 games added at once
-    """
-    if len(games) == 0:
-        await ctx.send("No game provided.")
-        return
-    elif len(games) > 10:
-        numgames = 10
-    else:
-        numgames = len(games)
-    for i in range(numgames):
-        for game in gameObj.get_games():
-            if games[i].lower() == game.lower() or games[i].lower() in gameObj.get_games()[game]:
-                role_to_add = discord.utils.get(ctx.guild.roles, name=game)
-                if role_to_add is not None:
-                    await ctx.author.add_roles(role_to_add)
-
-@bot.command()
-async def removegame(ctx, *games):
-    """
-    Removes game(s) from your roles.
-
-    Usage: !removegame game1 game2 ...
-    Maximum 10 games removed at once
-    """
-    if len(games) == 0:
-        await ctx.send("No game provided.")
-        return
-    elif len(games) > 10:
-        numgames = 10
-    else:
-        numgames = len(games)
-    for i in range(numgames):
-        for game in gameObj.get_games():
-            if games[i].lower() == game.lower() or games[i].lower() in gameObj.get_games()[game]:
-                role_to_rem = discord.utils.get(ctx.guild.roles, name=game)
-                if role_to_rem is not None:
-                    await ctx.author.remove_roles(role_to_rem)
-
-@bot.command()
-async def addall(ctx):
-    """
-    Adds all games to your roles.
-    UNLIMITED POWAHHHH
-    """
-    for game in gameObj.get_games():
-        role_to_add = discord.utils.get(ctx.guild.roles, name=game)
-        if role_to_add is not None:
-            await ctx.author.add_roles(role_to_add)
-
-@bot.command()
-async def removeall(ctx):
-    """
-    Removes all games from your roles.
-    Et ego dabo vobis tabula rasa...
-    """
-    for game in gameObj.get_games():
-        role_to_rem = discord.utils.get(ctx.guild.roles, name=game)
-        if role_to_rem is not None:
-            await ctx.author.add_roles(role_to_rem)
 
 @bot.command()
 @commands.check(no_reminder)
